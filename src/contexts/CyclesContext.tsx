a6499610c6338f5e5404d40b0cfa93379b9c6ad7
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-10-31 22:30:22 -0300

    Fix when the user enter for the first time in the application

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -38,12 +38,13 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
+  const INITIAL_STATE = {
+    cycles: [],
+    activeCycleId: null,
+  };
   const [cyclesState, dispatch] = useReducer(
     cyclesReducer,
-    {
-      cycles: [],
-      activeCycleId: null,
-    },
+    INITIAL_STATE,
     () => {
       const storedStateAsJSON = localStorage.getItem(
         "@ignite-timer:cycles-state-1.0.0"
@@ -52,6 +53,8 @@ export function CyclesContextProvider({
       if (storedStateAsJSON) {
         return JSON.parse(storedStateAsJSON);
       }
+
+      return INITIAL_STATE;
     }
   );
 
